5008a8f1b13ea08ce91a30f513c37816d292189f
ThePhiri <chipoephiri@gmail.com>
2023-07-27 16:41:07 +0200

    add unit test for create account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,11 +1,27 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestAccount(t *testing.T) {
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
 		Owner:    "John",
 		Balance:  100,
 		Currency: "ZAR",
 	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
 }
